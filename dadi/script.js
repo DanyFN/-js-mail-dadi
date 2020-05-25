05c3645801ad3891049b9420823fcf891025152c
Daniele <danielefontana90@gmail.com>
2020-05-25 14:29:27 +0200

    Commenti es dadi

--- dadi/script.js
+++ dadi/script.js
@@ -5,13 +5,14 @@
 // Generare un numero random da 1  a 6, sia per il giocatore sia per il computer
 // Stabilire il vincitore, in base a chi fa il punteggio più alto.
 
+//inizializzo variabili dadi che mi restituiscano randomico un numero da 1 a 6
 var dado1 = Math.floor(Math.random() * 6) + 1;
 console.log("DADO 1 TOTALIZZA " + dado1 +" PUNTO/I");
 var dado2 = Math.floor(Math.random() * 6) + 1;
 console.log("DADO 2 TOTALIZZA " + dado2 +" PUNTO/I");
 
 
-
+// condizioni del gioco
 if (dado1 > dado2) {
   document.writeln("Dado1 vince");
 }else if (dado2 > dado1) {
